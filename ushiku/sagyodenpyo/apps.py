5565142c7c083cd765b28abb35f10546ad45f773
hueno0429 <h.ueno@ueno.daiichi-koudai.ac.jp>
2025-03-04 23:02:10 +0900

    [ADD] New ushiku

--- ushiku/sagyodenpyo/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class SagyodenpyoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sagyodenpyo'
-
-    def ready(self):
-        import sagyodenpyo.signals
